352ae69a10ad1bece674c834c3746f884e864771
Micah Dattel <micahdattel@gmail.com>
2026-07-01 17:13:40 -0700

    adding all

---
+++ Script.js
@@ -0,0 +1,12 @@
+function blahblah(){
+    console.log("function ran")
+    return 0
+}
+let myvar = blahblah()
+
+console.log(myvar);
+if(myvar == 0){
+console.log("It's True")
+} else{console.log("Its False")}
+
+Math.random()
